3d0a66a22ef21f4b97f91cbe6ccdd69a9a72312f
Alex <alexmbroughton96@hotmail.co.uk>
2026-08-08 18:15:12 +0100

    Fix flake8 formatting issues

--- app/app.py
+++ app/app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         name = request.form.get("name")
         return render_template("index.html", name=name)
     return render_template("index.html", name=None)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
