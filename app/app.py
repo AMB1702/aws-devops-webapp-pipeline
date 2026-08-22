274a9fc541db2e344550f36d17616e7af0c0f034
Alex <alexmbroughton96@hotmail.co.uk>
2026-08-22 14:44:16 +0100

    Switch Flask back to port 5000

--- app/app.py
+++ app/app.py
@@ -12,6 +12,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 "# trigger test"
 "# trigger app"
